46238e92524cd078ae40268d30db39d9c076071c
Richard Bramley <rabramley@gmail.com>
2013-12-29 10:55:54 +0000

    Change directories

--- dopvr.sh
+++ dopvr.sh
@@ -5,7 +5,11 @@ echo "About to do the PVR thing"
 mkdir /tmp/recordings
 /usr/bin/get_iplayer --pvr --output "/tmp/recordings/" 2>>/tmp/get_iplayer.log
 
+echo "Got the Iplayer stuff"
+
 cd /tmp/recordings
+
+echo "About to convert files to MP3"
 
 for f in *.m4a; do
   filename=$(basename "$f")
@@ -17,9 +21,9 @@ for f in *.m4a; do
   album=`id3v2 -l $filename.mp3|grep TALB|cut -d':' -f2-| tr -d  [:space:] | tr -d [:punct:]`
   title=`id3v2 -l $filename.mp3|grep TIT2|cut -d':' -f2-| tr -d  [:space:] | tr -d [:punct:]`
 
-#  echo $performer
-#  echo $album
-#  echo $title
+  echo $performer
+  echo $album
+  echo $title
 
   mkdir --parents /home/richard/networkdrive/Multimedia/bbc/$performer/$album
   cp $filename.mp3 /home/richard/networkdrive/Multimedia/bbc/$performer/$album
@@ -31,6 +35,7 @@ for f in *.m4a; do
 
 done
 
+echo "Converted files to MP3"
 rmdir /tmp/recordings
 
 echo "Done the PVR thing"
