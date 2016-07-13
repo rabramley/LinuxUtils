4fc59aba06ade3d686e454f723a38e8fc423845c
Richard Bramley <rabramley@gmail.com>
2016-07-13 12:52:40 +0100

    Provide links to latest backups

--- backupAllDatabases.sh
+++ backupAllDatabases.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
- 
+
 if [[ $1 == D ]]; then
     BACKUP_PERIOD="daily"
     REMOVE_TIMESPAN_DAYS=+6
@@ -16,17 +16,27 @@ fi
 
 TIMESTAMP=$(date +"%F")
 BACKUP_DIR="/local/backup/$BACKUP_PERIOD"
-MYSQL_USER="username"
+LATEST_DIR="/local/backup/Latest"
+MYSQL_USER="lampuser"
 MYSQL=/usr/bin/mysql
-MYSQL_PASSWORD="password"
+MYSQL_PASSWORD="PeRin1"
 MYSQLDUMP=/usr/bin/mysqldump
- 
+
 mkdir -p "$BACKUP_DIR"
- 
+
+if [[ $1 == D ]]; then
+    rm -fR "$LATEST_DIR"
+    mkdir -p "$LATEST_DIR"
+fi
+
 databases=`$MYSQL -S /db/mysql/mysql.sock --user=$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
- 
+
 for db in $databases; do
   $MYSQLDUMP --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases --events --routines --triggers -S /db/mysql/mysql.sock $db | gzip > "$BACKUP_DIR/$db-$TIMESTAMP.gz"
+
+  if [[ $1 == D ]]; then
+    ln -s "$BACKUP_DIR/$db-$TIMESTAMP.gz" "$LATEST_DIR/$db-$TIMESTAMP.gz"
+  fi
 done
 
 find $BACKUP_DIR -ctime $REMOVE_TIMESPAN_DAYS -exec rm {} +
